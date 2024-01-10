0a5b3e5c910c69299892e6650c0a57938fbdfad4
a.svirin <a.svirin94@gmail.com>
2024-01-10 16:52:58 +0300

    fixes

--- src/components/cards-list/cards-list.tsx
+++ src/components/cards-list/cards-list.tsx
@@ -1,30 +1,15 @@
-import { useParams } from 'react-router-dom';
 import { PRODUCTS_ON_PAGE_COUNT } from '../../consts';
-import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useAppSelector } from '../../hooks';
 import { getCurrentPage } from '../../store/app-process/app-process.selectors';
 import { getProducts } from '../../store/data-process/data-process.selectors';
 import ProductCard from '../product-card/product-card';
-import { useEffect } from 'react';
-import { setCurrentPage } from '../../store/app-process/app-process.slice';
 
 export default function CardsList() {
-  const dispatch = useAppDispatch();
   const products = useAppSelector(getProducts);
   const currentPage = useAppSelector(getCurrentPage);
 
-  const param = useParams().page;
-  const page = param
-    ? +param - 1
-    : currentPage;
-
-  useEffect(() => {
-    if(page !== currentPage) {
-      dispatch(setCurrentPage(page));
-    }
-  }, []);
-
   const productsOnPage = products.length > 9 && currentPage !== null
-    ? products.slice(page * PRODUCTS_ON_PAGE_COUNT, (page * PRODUCTS_ON_PAGE_COUNT + PRODUCTS_ON_PAGE_COUNT))
+    ? products.slice(currentPage * PRODUCTS_ON_PAGE_COUNT, (currentPage * PRODUCTS_ON_PAGE_COUNT + PRODUCTS_ON_PAGE_COUNT))
     : products;
 
   return (
--- src/pages/start-page/start-page.tsx
+++ src/pages/start-page/start-page.tsx
@@ -5,8 +5,25 @@ import Header from '../../components/header/header';
 import Modal from '../../components/modal/modal';
 import PageContent from '../../components/page-content/page-content';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { setCurrentPage } from '../../store/app-process/app-process.slice';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { getCurrentPage } from '../../store/app-process/app-process.selectors';
 
 export default function StartPage() {
+  const dispatch = useAppDispatch();
+  const currentPage = useAppSelector(getCurrentPage);
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const page = searchParams.get('page');
+
+  useEffect(() => {
+    if(page && +page !== currentPage) {
+      dispatch(setCurrentPage(+page - 1));
+    }
+  }, [page]);
+
   return (
     <div className='wrapper'>
       <Helmet><title>Фотошоп</title></Helmet>
