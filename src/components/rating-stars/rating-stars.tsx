f1efd1b76e6cdb163629b5f695cf3b6339f4def3
a.svirin <a.svirin94@gmail.com>
2024-01-10 16:18:25 +0300

    rating component fix

--- src/components/rating-stars/rating-stars.tsx
+++ src/components/rating-stars/rating-stars.tsx
@@ -2,20 +2,27 @@ import { STARS_COUNT } from '../../consts';
 
 type Props = {
   rating: number;
-}
+};
 
-export default function RatingStars({rating}: Props) {
-  const result = [];
+export default function RatingStars({ rating }: Props): JSX.Element {
+  const ratingElement = () => {
+    const result = [];
 
-  for (let i = 1; i <= STARS_COUNT; i++) {
-    const starType = rating >= i ? '#icon-full-star' : '#icon-star';
+    for (let i = 1; i <= STARS_COUNT; i++) {
+      const starType = rating >= i ? '#icon-full-star' : '#icon-star';
 
-    result.push(
-      <svg width="17" height="16" aria-hidden="true" key={i}>
-        <use xlinkHref={starType}></use>
-      </svg>
-    );
-  }
+      result.push(
+        <svg width="17" height="16" aria-hidden="true" key={i}>
+          <use xlinkHref={starType}></use>
+        </svg>
+      );
+    }
 
-  return result;
+    return result;
+  };
+  return (
+    <div>
+      {ratingElement()}
+    </div>
+  );
 }
