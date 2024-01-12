b9ddddc8ed295ac0fc9cae02d48659cf1574f513
a.svirin <a.svirin94@gmail.com>
2024-01-12 17:52:30 +0300

    added link to main page on not found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
     <div>
+      <Link
+        className="header__logo"
+        to='/'
+        aria-label="Переход на главную"
+      >
+        <svg width="100" height="36" aria-hidden="true">
+          <use xlinkHref="#icon-logo"></use>
+        </svg>
+      </Link>
       <p>404 not found</p>
     </div>
   );
