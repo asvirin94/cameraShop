afd033206d8f2dc64c14fb2bb4586a4c1f540dc6
a.svirin <a.svirin94@gmail.com>
2024-01-12 17:49:51 +0300

    fixed pagination position

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -9,16 +9,18 @@ import { getPromos } from '../../store/data-process/data-process.selectors';
 import { Link } from 'react-router-dom';
 import { AppRoutes } from '../../consts';
 
-
 export default function Banner() {
-
   const promos = useAppSelector(getPromos);
 
   return (
-    <div className='banner'>
+    <div className="banner">
       <Swiper
         effect={'creative'}
-        autoplay={{ delay: 3000, disableOnInteraction: false, pauseOnMouseEnter: true}}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+        }}
         loop
         creativeEffect={{
           prev: {
@@ -31,25 +33,27 @@ export default function Banner() {
           },
         }}
         modules={[EffectCreative, Autoplay, Pagination]}
-        pagination={{ clickable: true}}
-        style={{
-          height: '280px',
-          '--swiper-pagination-color': '#7575E2',
-          '--swiper-pagination-bullet-inactive-color': '#F4F4FC',
-          '--swiper-pagination-bullet-size': '14px',
-          '--swiper-pagination-bottom': '22px',
-          '--swiper-pagination-left': '800px'
-        } as React.CSSProperties}
+        pagination={{ clickable: true, el: '.swiper-pagination' }}
+        style={
+          {
+            height: '280px',
+            width: '100%',
+            position: 'relative',
+            '--swiper-pagination-color': '#7575E2',
+            '--swiper-pagination-bullet-inactive-color': '#F4F4FC',
+            '--swiper-pagination-bullet-inactive-opacity': '1',
+            '--swiper-pagination-bullet-size': '16px',
+            '--swiper-pagination-bottom': '22px',
+            '--swiper-pagination-bullet-horizontal-gap': '0',
+          } as React.CSSProperties
+        }
       >
         <div className="swiper-wrapper">
           {promos.map((promo) => (
             <SwiperSlide key={promo.id}>
               <div className="swiper-slide">
                 <picture>
-                  <source
-                    type="image/webp"
-                    srcSet={promo.previewImgWebp}
-                  />
+                  <source type="image/webp" srcSet={promo.previewImgWebp} />
                   <img
                     src={promo.previewImg}
                     srcSet={promo.previewImgWebp2x}
@@ -60,19 +64,25 @@ export default function Banner() {
                 </picture>
                 <p className="banner__info">
                   <span className="banner__message">Новинка!</span>
-                  <span className="title title--h1">
-                    {promo.name}
-                  </span>
+                  <span className="title title--h1">{promo.name}</span>
                   <span className="banner__text">
-              Профессиональная камера от&nbsp;известного производителя
+                    Профессиональная камера от&nbsp;известного производителя
                   </span>
-                  <Link className="btn" to={`/${AppRoutes.Product}${promo.id}/description`}>
-              Подробнее
+                  <Link
+                    className="btn"
+                    to={`/${AppRoutes.Product}${promo.id}/description`}
+                  >
+                    Подробнее
                   </Link>
                 </p>
               </div>
             </SwiperSlide>
           ))}
+        </div>
+        <div
+          className="swiper-pagination"
+          style={{ position: 'absolute', width: '74px', right: '40px', left: 'auto', display: 'flex', justifyContent: 'space-between' }}
+        >
         </div>
       </Swiper>
     </div>
